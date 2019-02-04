d726f5015ff394163ec56fec334d19e283ad28c2
ElizaHlglh <ewu4@stuy.edu>
2019-02-04 11:39:11 -0500

    classwork.java

---
+++ classwork.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+public class classwork{
+  public static List<String> makeAllWords(int k, int maxLetter){ //classwork 2/4/19
+    List<String> ans = new ArrayList<String>();
+
+    return ans;
+  }
+
+  public void makeWordsHelp(int k, String word){
+    if (k == 0){
+
+    }
+    else{
+      
+    }
+  }
+
+}
