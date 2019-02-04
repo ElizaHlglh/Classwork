b183dddf9f2a6d7cf6eecca3477b37ad7e4332c3
ElizaHlglh <ewu4@stuy.edu>
2019-02-04 11:47:42 -0500

    classwork.java

--- classwork.java
+++ classwork.java
@@ -3,16 +3,18 @@ import java.util.ArrayList;
 public class classwork{
   public static List<String> makeAllWords(int k, int maxLetter){ //classwork 2/4/19
     List<String> ans = new ArrayList<String>();
-
+    makeWordsHelp(k, "", ans, maxLetter);
     return ans;
   }
 
-  public void makeWordsHelp(int k, String word){
+  public static void makeWordsHelp(int k, String word, List<String> ans, int max){
     if (k == 0){
-
+      ans.add(word);
     }
     else{
-      
+      for (int i = 'a' + 0; i < 'a' + max; i++){
+        makeWordsHelp(k-1, word + (char) i, ans, max);
+      }
     }
   }
 
