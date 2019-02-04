1d65244e102ce0bb8a3cb4a681ef7167e8ca3eeb
ElizaHlglh <ewu4@stuy.edu>
2019-02-04 16:07:05 -0500

    classwork.java

--- driver.java
+++ driver.java
@@ -1,6 +1,7 @@
 public class driver{
   public static void main(String[] args){
     System.out.println("Testing makeAllWords(k, maxLetter):");
-    System.out.println("sqrt(2), Expecting: 1.4142.   Getting: " + recursion.sqrt(2,0.001));
+    System.out.println("makeAllWords(1,3), Expecting: [a,b,c]   Getting: " + classwork.makeAllWords(1,3));
+    System.out.println("makeAllWords(2,3), Expecting: [aa,ab,ac,ba,bb,bc,ca,cb,cc]   Getting: " + classwork.makeAllWords(2,3));
   }
 }
